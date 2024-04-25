b2196c92ad33987ab1c6f4f0d7f5653aaa70f286
design-proto-build <55461751+design-proto-build@users.noreply.github.com>
2024-04-25 18:05:56 +0000

    My Solution for Exercise 2

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,29 @@
-// Write your code here
+// My Solution
+
+class Movie {
+  constructor(title, director, genre, releaseYear, rating) {
+    this.title = title;
+    this.director = director;
+    this.genre = genre;
+    this.releaseYear = releaseYear;
+    this.rating = rating;
+  }
+
+  get overview() {
+    return this.getOverview();
+  }
+
+  getOverview = function() {
+    return `${this.title}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}`
+  }
+}
+
+let batman_begins = new Movie(
+  "Batman Begins",
+  "Christopher Nolan",
+  "Action",
+  "2008",
+  "4.5/5"
+)
+console.log(batman_begins.getOverview());
+console.log(batman_begins.overview);
